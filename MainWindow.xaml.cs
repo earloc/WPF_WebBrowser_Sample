abdfd8a029f64fa44d66875750a24b95b3cbfd0e
earloc <earloc@live.de>
2017-05-11 11:59:29 +0200

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using mshtml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebBrowser_HTML {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void WebBrowser_Navigated(object sender, NavigationEventArgs e) {
+            var document = (HTMLDocument)_Browser.Document;
+
+            _Html.Text = document.body.outerHTML;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
